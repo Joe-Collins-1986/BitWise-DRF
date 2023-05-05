03dab3503b91d51f4bc72ab3f4f7714c05fb361a
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-05 14:22:00 +0100

    add views to article

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from bitwise.permissions import IsOwnerOrReadOnly
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+class ArticleList(generics.ListCreateAPIView):
+    """
+    - List out all the articles
+    - Option to create new aricles if logged in
+    with owner = request.user
+    """
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Article.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    - Detail the specificly requested article
+    - Uses same Article serializer
+    - Uses IsOwnerOrReadOnly tailored permission class
+    to ensure only owner can update or delete article info
+    """
+    serializer_class = ArticleSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Article.objects.all()
