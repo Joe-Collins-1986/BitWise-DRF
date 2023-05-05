040d1f5776d9f23b49d0f67cebcce825c34cb79b
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-05 14:55:14 +0100

    add comment model

---
+++ comments/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from articles.models import Article
+
+
+class Comment(models.Model):
+    """
+    Article Model:
+    Foreign Key - User, Article
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.body
