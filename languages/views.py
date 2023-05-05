85ff435db70684f5d023b3f1c7dd84a3ae763b5c
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-05 12:50:30 +0100

    build language views and serializers, calc years_exp and validate language duplication

--- languages/views.py
+++ languages/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from bitwise.permissions import IsOwnerOrReadOnly
+from .models import Language
+from .serializers import LanguageDetailSerializer, LanguageSerializer
 
-# Create your views here.
+class LanguageList(generics.ListCreateAPIView):
+    serializer_class = LanguageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Language.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LanguageDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LanguageDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Language.objects.all()
