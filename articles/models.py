4ab649453a1bce4f0db261a8fd750876ac08d93f
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-05 14:16:19 +0100

    add articles model

---
+++ articles/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from languages.choices import LANGUAGE_CHOICES
+
+
+class Article(models.Model):
+    """
+    Article Model:
+    Foreign Key - User
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    article_title = models.CharField(max_length=255)
+    article_content = models.TextField(blank=True)
+    primary_language = models.CharField(
+        max_length=25,
+        choices=LANGUAGE_CHOICES,
+        blank=True,
+    )
+    github_link = models.URLField(blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.article_title}'
--- bitwise/settings.py
+++ bitwise/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'profiles',
+    'languages',
+    'articles',
 
 ]
 
