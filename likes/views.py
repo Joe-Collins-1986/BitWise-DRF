fe5fa7a5430b18d043e0229fdf308bf6e1a054bb
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-05 15:30:44 +0100

    add like views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from bitwise.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+class LikeList(generics.ListCreateAPIView):
+    """
+    - List out all the likes
+    - Option to create like if logged in
+    with owner = request.user
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    - Detail the specificly requested like
+    - Uses same Like serializer
+    - Uses IsOwnerOrReadOnly tailored permission class
+    to ensure only owner can delete article info
+    - No need for update, likes works of deleting and 
+    creating a new like if required
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
