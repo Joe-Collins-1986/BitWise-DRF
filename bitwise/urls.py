12e9481e4b16291513f638c427cd7f9200681cc6
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-06 14:45:10 +0100

    add api documentation

--- bitwise/urls.py
+++ bitwise/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+    path('', include_docs_urls(title="BitWise Documentation")),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
