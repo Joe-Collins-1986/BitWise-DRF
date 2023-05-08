782807cdd1676fe2606de2d331ab45686bf52137
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-08 11:15:34 +0100

    add loutout JWT aut views

--- bitwise/urls.py
+++ bitwise/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from .views import logout_route
 
 urlpatterns = [
     path('', include_docs_urls(title="BitWise Documentation")),
     path('admin/', admin.site.urls),
 
     path('api-auth/', include('rest_framework.urls')),
+
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
+    
     path('profiles/', include('profiles.urls')),
     path('languages/', include('languages.urls')),
     path('articles/', include('articles.urls')),
