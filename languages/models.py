9c1e09135f2555303877a6b5befc1e14088405a4
Joseph Collins <joecollins@Josephs-MBP.home>
2023-06-06 11:54:44 +0100

    update language model to mandatory fields and wider confidence spread

--- languages/models.py
+++ languages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .choices import LANGUAGE_CHOICES, CONFIDENCE_LEVEL
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Language(models.Model):
@@ -11,15 +11,11 @@ class Language(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="languages")
     language = models.CharField(
         max_length=25,
-        # choices=LANGUAGE_CHOICES,
-        blank=True,
         )
-    confidence = models.CharField(
-        max_length=25,
-        choices=CONFIDENCE_LEVEL,
-        blank=True,
-        )
-    used_since = models.DateField(null=True, blank=True)
+    confidence = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)],
+    )
+    used_since = models.DateField(null=True)
 
     class Meta:
         ordering = ['-confidence']
