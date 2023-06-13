b6bab800b93ceb374bc16280f8492991e5114d3f
Joseph Collins <joecollins@Josephs-MBP.home>
2023-06-13 12:47:31 +0100

    add bitwise views unit test

---
+++ bitwise/test_bitwise_views.py
@@ -0,0 +1,31 @@
+from rest_framework.test import APITestCase
+
+class LogoutRouteTest(APITestCase):
+    def test_logout_route(self):
+        response = self.client.post("/dj-rest-auth/logout/")
+        
+        '''
+        response status code is 200 (OK)
+        '''
+        self.assertEqual(response.status_code, 200)
+
+        '''
+        JWT_AUTH_COOKIE and JWT_AUTH_REFRESH_COOKIE cookies are cleared
+        '''
+        self.assertEqual(response.cookies['app-auth'].value, "")
+        self.assertEqual(response.cookies['refresh-token'].value, "")
+
+        '''
+        cookies have the expected attributes
+        '''
+        self.assertTrue(response.cookies['app-auth']["httponly"])
+        self.assertEqual(response.cookies['app-auth']['expires'], 'Thu, 01 Jan 1970 00:00:00 GMT')
+        self.assertEqual(response.cookies['app-auth']['max-age'], 0)
+        self.assertEqual(response.cookies['app-auth']['samesite'], 'None')
+        self.assertEqual(response.cookies['app-auth']['secure'], True)
+
+        self.assertTrue(response.cookies['refresh-token']["httponly"])
+        self.assertEqual(response.cookies['refresh-token']['expires'], 'Thu, 01 Jan 1970 00:00:00 GMT')
+        self.assertEqual(response.cookies['refresh-token']['max-age'], 0)
+        self.assertEqual(response.cookies['refresh-token']['samesite'], 'None')
+        self.assertEqual(response.cookies['refresh-token']['secure'], True)
